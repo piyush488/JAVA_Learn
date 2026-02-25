aeabc6bf052361551b0008328622069ae8d9bfa0
Piyush <piyushsrivastava488@email.com>
2026-02-25 17:03:26 +0530

    Added Java Day 2

---
+++ JAVA DAY_2/PR_6.java
@@ -0,0 +1,13 @@
+// ✅Store 2 numbers and print sum
+
+public class PR_6{
+    public static void main(String[] args){
+
+        int a=10;
+        int b=20;
+
+        int sum = a + b;
+
+        System.out.println("the sum of 2 numbers are :" +sum);
+    }
+}
