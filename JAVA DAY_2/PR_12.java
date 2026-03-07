56d18da6a74dd273432d82096f821b24a3d090d2
Piyush <piyushsrivastava488@email.com>
2026-03-07 21:37:16 +0530

    PR_12 added

---
+++ JAVA DAY_2/PR_12.java
@@ -0,0 +1,13 @@
+// print avg of 3 numbers 
+
+public class PR_12 {
+
+    public static void main(String[] args){
+        int num1=10;
+        int num2=20;
+        int num3=30;
+        int avg=(num1+num2+num3)/3;
+        System.out.println("Average: "+avg);
+    }
+    
+}
